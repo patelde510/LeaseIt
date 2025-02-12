b7b7a8a86a59004a48330bf822ea1d1cf6371fcc
Vansh Patel <pvansh30@gmail.com>
2025-02-12 17:17:10 -0500

    putting SSL back

--- app/server.js
+++ app/server.js
@@ -16,9 +16,9 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
   // USE BELOW TO TEST FOR PRODUCTION ONLY
-  // ssl: {
-  //   rejectUnauthorized: false
-  // }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 app.use(express.static(path.join(__dirname, "public")));
