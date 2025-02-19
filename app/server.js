ac29dc0f3bbfa49af2474efc8fea621856aff8c7
Vansh Patel <pvansh30@gmail.com>
2025-02-18 19:00:39 -0500

    comment out

--- app/server.js
+++ app/server.js
@@ -19,9 +19,9 @@ const pool = new Pool({
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT || 5432,
   // COMMENT OUT THE PART BELOW WHEN USING A LOCAL DATABASE
-  //ssl: {
-  //  rejectUnauthorized: false
-  //}
+  ssl: {
+   rejectUnauthorized: false
+  }
 });
 
 const s3 = new AWS.S3({
